461511936f04dfa7680b5a28302b33487896ac76
150adrian <47202663+150adrian@users.noreply.github.com>
2019-04-10 20:19:17 -0700

    Act2
    Activity2

--- Activity2/Definite_Integral.py
+++ Activity2/Definite_Integral.py
@@ -6,6 +6,7 @@ def definite_integral(samples, lower_limit, upper_limit, sampling_period):
 
     #tratando de aplicar la formula, veremos si funciona el for que vaya incrementando de acuerdo al rango que se pide del sampling period
     #Una formula para los primer y ultimo valor ya que estos no estan  multiplicados
+    Formula1 =  (sampling_period/2) * (lower_limit +  upper_limit)
     Formula2 = 0
     print("Valor de la formuala 1 carnal", Formula1)
     for Values in range(samples[1], samples[-1], sampling_period):
