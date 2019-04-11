8880d0b9fcaf298cda97f551ee9579f8394eaeae
150adrian <47202663+150adrian@users.noreply.github.com>
2019-04-10 20:11:58 -0700

    Create Definite_Integral.py
    Definte_Integral

---
+++ AcceGyro/Definite_Integral.py
@@ -0,0 +1,17 @@
+def definite_integral(samples, lower_limit, upper_limit, sampling_period):
+    #DEFINIMOD LA ECUACION PARA LA Trapezoidal Integration 
+    upper_limit = samples[-1]
+    lower_limit = samples[0]
+   
+
+    #tratando de aplicar la formula, veremos si funciona el for que vaya incrementando de acuerdo al rango que se pide del sampling period
+    #Una formula para los primer y ultimo valor ya que estos no estan  multiplicados
+    Formula2 = 0
+    print("Valor de la formuala 1 carnal", Formula1)
+    for Values in range(samples[1], samples[-1], sampling_period):
+    	print(Values)
+    	summ = (sampling_period/2) * (2 * Values)
+    	Formula2 += summ
+    	FinalResult = Formula2 +  Formula1
+    	print(FinalResult)
+
